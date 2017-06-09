7bb16d4e4153a71ff09f0ee75aa112472529d7dd
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-09 15:25:45 -0400

    add ListToggle comp. Property intializer syntax to bind callbacks

--- src/App.js
+++ src/App.js
@@ -187,11 +187,36 @@ class Item extends React.Component {
             <div className="title">{this.props.title}</div>
             <div className="rating">{this.props.score} / 10</div>
             <div className="plot">{this.props.overview}</div>
-            {/*<ListToggle />*/}
+            <ListToggle />
           </div>
         </div>
     );
   }
 }
 
+// List Toggle 
+class ListToggle extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      toggled: false
+    }
+  } 
+  // property initializer syntax to correctly bind callbacks. 
+  handleClick = () => {
+    (this.state.toggled) ? this.setState({ toggled: false}) : this.setState({ toggled: true }); 
+  }
+  render() {
+    return (
+      <div onClick={this.handleClick} data-toggled={this.state.toggled} className="ListToggle">
+        <div>
+          <i className="fa fa-fw fa-plus"></i>
+          <i className="fa fa-fw fa-check"></i>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './App.css';
 import './index.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
